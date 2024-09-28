3a9a9dcc1d0a49de10c471fbdc9d2c6844092354
Rasenkai <rasenkai99@gmail.com>
2024-09-28 13:39:07 +0530

    {social-media-monitor/backend}: RIP instagram
    Signed-off-by: Rasenkai <rasenkai99@gmail.com>

--- backend/services/scraper.js
+++ backend/services/scraper.js
@@ -1,13 +1,10 @@
 const { scrapeTwitter } = require('./twitterScraper');
-const { scrapeInstagram } = require('./instagramScraper');
 const { scrapeTelegram } = require('./telegramScraper');
 
 async function scrapeContent(platform, keyword, isLoggedIn) {
   switch (platform) {
     case 'twitter':
       return await scrapeTwitter(keyword, isLoggedIn);
-    case 'instagram':
-      return await scrapeInstagram(keyword, isLoggedIn);
     case 'telegram':
       return await scrapeTelegram(keyword);
     default:
